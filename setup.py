79e9161ad2ffcc8c8cc187543c5fee2fe0969944
OfAaron3 <aaronwpeat@googlemail.com>
2023-03-21 14:23:52 +0000

    versioning

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
         return f.read()
 
 setup(name='irispreppy',
-      version="1.1.0",
+      version="1.1.1",
       url='https://github.com/OfAaron3/irispreppy',
       author='Aaron W. Peat',
       author_email='a.peat.1@research.gla.ac.uk',
