34f2cb6e77b21c3b7fd076b2cf3873cd9296b6d8
OfAaron3 <aaronwpeat@googlemail.com>
2025-09-25 14:49:42 +0200

    Clearing gh-pages branch

--- irispreppy/__init__.py
+++
@@ -1,6 +0,0 @@
-from .psf.deconvolve import deconvolve
-from .psf.decsave import deconvolve_and_save
-from .radcal.radcal import radiometric_calibrate
-from .radcal.iris_get_response import iris_get_response
-from .radcal.calsave import calibrate_and_save
-from .version import __version__ 
