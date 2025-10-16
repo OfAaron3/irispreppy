cfafcf36280710e1609ccd48576ee64761db067b
Nabil Freij <nabil.freij@gmail.com>
2025-10-16 14:16:26 -0700

    Run pre-commit

--- irispreppy/__init__.py
+++ irispreppy/__init__.py
@@ -3,4 +3,4 @@ from .psf.decsave import deconvolve_and_save
 from .radcal.radcal import radiometric_calibrate
 from .radcal.iris_get_response import iris_get_response
 from .radcal.calsave import calibrate_and_save
-from .version import __version__ 
+from .version import __version__
